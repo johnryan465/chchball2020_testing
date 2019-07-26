be6484afec81bf0a67f1d1b9c471a1147e96a24c
John Ryan <johryan@johryan-mn1.linkedin.biz>
2019-07-26 07:57:30 +0100

    A bunch of stuff

--- env.js
+++ env.js
@@ -1,7 +1,29 @@
 $(function(){
+  function whichAnimationEvent(){
+  var t,
+      el = document.createElement("fakeelement");
+
+  var animations = {
+    "animation"      : "animationend",
+    "OAnimation"     : "oAnimationEnd",
+    "MozAnimation"   : "animationend",
+    "WebkitAnimation": "webkitAnimationEnd"
+  }
+
+  for (t in animations){
+    if (el.style[t] !== undefined){
+      return animations[t];
+    }
+  }
+}
+
+var animationEvent = whichAnimationEvent();
   $('.envelope').click(function(){
     if(!$('.envelope').hasClass('open')){
       $(this).removeClass('new').addClass('open');
+    }
+    if(!$('.clock').hasClass('hidden')){
+      $(this).removeClass('hidden').addClass('show');
     }
   });
 });
--- stopwatch.js
+++ stopwatch.js
@@ -1,1 +1,21 @@
-var s = skrollr.init();
+var inc = 1000;
+
+clock();
+
+function clock() {
+  const date = new Date();
+
+  const hours = ((date.getHours() + 11) % 12 + 1);
+  const minutes = date.getMinutes();
+  const seconds = date.getSeconds();
+
+  const hour = hours * 30;
+  const minute = minutes * 6;
+  const second = seconds * 6;
+
+  document.querySelector('.hour-hand').style.transform = `rotate(${hour}deg)`
+  document.querySelector('.minute-hand').style.transform = `rotate(${minute}deg)`
+  document.querySelector('.seconds-hand').style.transform = `rotate(${second}deg)`
+}
+
+setInterval(clock, inc);
