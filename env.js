dcedc0bb94c77a5a05d512b4b5cdc762bda4477b
John Ryan <johryan@johryan-mn1.linkedin.biz>
2019-07-27 13:14:11 +0100

    Code clean up

--- env.js
+++ env.js
@@ -5,34 +5,21 @@ $(function(){
       $('.slide-in').addClass('show');
       $(this).off(e);
    });
+   //When the letter has finished we want to scale up the image
    $(".letter")
    .on("animationend webkitAnimationEnd oAnimationEnd MSAnimationEnd",
     function(e){
       var $doc = $(document);
-      var centerX = $doc.width() / 2;
-      var centerY = $doc.height() / 2;
       var scale = Math.min($doc.width()/$(this).width(),$doc.height()/$(this).height())
-      $("#inv").css({
-        "transform": "scale(" + scale + ")",
-        "transition-duration": "0.5s"
-      });
-      console.log(scale)
+      document.body.style.setProperty("--scale-factor", scale);
+      $("#inv").addClass("full-screen")
       $(this).off(e);
     });
   $('.envelope').click(function(){
     if(!$('.envelope').hasClass('open')){
       $(this).removeClass('new').addClass('open');
+      $('.seal').addClass('flipped');
     }
-    $('.seal').addClass('flipped');
-    $('.slide-in').css('z-index', 10);
-    $('.slide-in').css('overflow', 'visible');
-    var $doc = $(document);
-    var centerX = $doc.width() / 2;
-    var centerY = $doc.height() / 2;
-    $("#inv").css({
-      "transform": "translateX(" + centerX + ")",
-      "transition-duration": "0.5s"
-    });
     $(this).off();
   });
 });
