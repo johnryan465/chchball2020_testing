257f8460e488c9add0501207ed88147c09fe7018
John Ryan <johryan@johryan-mn1.linkedin.biz>
2019-07-27 09:59:22 +0100

    Scaling

--- env.js
+++ env.js
@@ -1,4 +1,5 @@
 $(function(){
+
   function whichTransitionEvent(){
     var t;
     var el = document.createElement('fakeelement');
@@ -23,6 +24,23 @@ $(function(){
       $('.slide-in').addClass('show');
       $(this).off(e);
    });
+   $(".letter")
+   .on("animationend webkitAnimationEnd oAnimationEnd MSAnimationEnd",
+    function(e){
+      var $doc = $(document);
+      var centerX = $doc.width() / 2;
+      var centerY = $doc.height() / 2;
+      var scale = Math.min($doc.width()/$(this).width(),$doc.height()/$(this).height())
+      var $card = $(this);
+      var origX = $card.data('orig-x');
+      var origY = $card.data('orig-y');
+      $("#inv").css({
+        "transform": "scale(" + scale + ")",
+        "transition-duration": "0.5s"
+      });
+      console.log(scale)
+      $(this).off(e);
+    });
   $('.envelope').click(function(){
     if(!$('.envelope').hasClass('open')){
       $(this).removeClass('new').addClass('open');
