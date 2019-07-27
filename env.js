03014a4cf7f43227f0ed8fcea752121f11c350bb
John Ryan <johryan@johryan-mn1.linkedin.biz>
2019-07-27 11:43:59 +0100

    Added gold foil + effects

--- env.js
+++ env.js
@@ -1,23 +1,4 @@
 $(function(){
-
-  function whichTransitionEvent(){
-    var t;
-    var el = document.createElement('fakeelement');
-    var transitions = {
-      'WebkitTransition' :'webkitTransitionEnd',
-      'MozTransition'    :'transitionend',
-      'MSTransition'     :'msTransitionEnd',
-      'OTransition'      :'oTransitionEnd',
-      'transition'       :'transitionEnd'
-    }
-
-    for(t in transitions){
-      if( el.style[t] !== undefined ){
-        return transitions[t];
-      }
-    }
-  }
-
   $(".top-flap")
   .on("animationend webkitAnimationEnd oAnimationEnd MSAnimationEnd",
    function(e){
@@ -31,9 +12,6 @@ $(function(){
       var centerX = $doc.width() / 2;
       var centerY = $doc.height() / 2;
       var scale = Math.min($doc.width()/$(this).width(),$doc.height()/$(this).height())
-      var $card = $(this);
-      var origX = $card.data('orig-x');
-      var origY = $card.data('orig-y');
       $("#inv").css({
         "transform": "scale(" + scale + ")",
         "transition-duration": "0.5s"
@@ -48,5 +26,13 @@ $(function(){
     $('.seal').addClass('flipped');
     $('.slide-in').css('z-index', 10);
     $('.slide-in').css('overflow', 'visible');
+    var $doc = $(document);
+    var centerX = $doc.width() / 2;
+    var centerY = $doc.height() / 2;
+    $("#inv").css({
+      "transform": "translateX(" + centerX + ")",
+      "transition-duration": "0.5s"
+    });
+    $(this).off();
   });
 });
